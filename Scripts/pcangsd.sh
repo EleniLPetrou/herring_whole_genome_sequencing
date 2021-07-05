ff6029590043444ec4c4acb0f8d0ef636fa7237b
Eleni L Petrou <elpetrou@gmail.com>
2021-07-05 11:53:23 -0700

    Add files via upload

---
+++ Scripts/pcangsd.sh
@@ -0,0 +1,35 @@
+# The purpose of this script is to calculate a covriance matrix for PCA.
+# It uses a beagle file created from angsd to do this.
+# For some odd reason I could only get pcangsd to work on an interactive node.
+
+# Request an interactive node on Klone
+srun -p compute-hugemem -A merlab --nodes=1 --ntasks-per-node=1 --time=02:00:00 --mem=100G --pty /bin/bash
+
+#####################################################################
+##### ENVIRONMENT SETUP ##########
+
+## software information
+MYCONDA=/gscratch/merlab/software/miniconda3/etc/profile.d/conda.sh # path to conda installation on our Klone node. Do NOT change this.
+MYENV=pcangsd_env #name of the conda environment containing samtools software. 
+PCANGSD=/gscratch/merlab/software/miniconda3/envs/pcangsd/pcangsd.py #path to pcangsd software on Klone
+
+## data information
+DATADIR=/gscratch/scrubbed/elpetrou/angsd #path to input files (beagle file)
+OUTDIR=/gscratch/scrubbed/elpetrou/angsd #path to output files
+MYINFILE=all_samples_maf0.05_miss0.3.nuclear.beagle.gz #name of beagle file
+OUTPREFIX=results_pca_all_samples_maf0.05_miss0.3.nuclear #prefix for output files
+
+##################################################################
+## Activate the conda environment:
+## start with clean slate
+module purge
+
+## This is the filepath to our conda installation on Klone. Source command will allow us to execute commands from a file in the current shell
+source $MYCONDA
+
+## activate the conda environment
+conda activate $MYENV
+
+## run pcangsd
+python $PCANGSD -beagle $DATADIR'/'$MYINFILE -o $OUTDIR'/'$OUTPREFIX -threads 8
+
